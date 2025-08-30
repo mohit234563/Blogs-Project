f48e9504804288158548b1aadc5c552a5960a0ee
mohit234563 <mohitvj721@gmail.com>
2025-08-30 17:12:05 +0530

    get one route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const router=require('./routes/studentsRouter');
 app.use(express.json());
 app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Backend is live 🚀");
+});
+
 app.use('/api/students',router);
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
