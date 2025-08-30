a0aa4ebe06a8834ce31980a6f3aab66995378eb4
mohit234563 <mohitvj721@gmail.com>
2025-08-30 17:29:55 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const router=require('./routes/studentsRouter');
 app.use(express.json());
 app.use(cors({
-  origin: "https://mohit234563.github.io/Blogs-Project/",
+  origin: "https://mohit234563.github.io/Blogs-Project",
   credentials: true
 }));
 app.get("/", (req, res) => {
